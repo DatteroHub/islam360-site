323db7fe54c4fd9267711766c1bd926b1f643dce
DatteroMaster <73127774+abouzahra@users.noreply.github.com>
2023-03-26 00:53:06 +0100

    fixed py for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ import ContentDiv from "../components/ContentDiv"
 export default function NotFound() {
   return (
     <Layout>
-      <BgDiv className="">
+      <BgDiv className="py-24 mt-12">
         <ContentDiv className="flex justify-center items-center gap-40">
           <div>
             <h1 className="font-semibold text-8xl md:text-9xl text-gray-700">404</h1>
@@ -16,7 +16,7 @@ export default function NotFound() {
           <img src={'/404.svg'} alt="Hero" className='h-56 xl:h-64 hidden lg:flex' />
         </ContentDiv>
       </BgDiv>
-      <BgDiv className="pt-8">
+      <BgDiv className="pt-8 pb-24">
         <ContentDiv className="flex justify-center">
           <Link to="/" className="text-medium text-gray-500 flex items-center gap-4 bg-neutral-200/50 hover:bg-neutral-200 py-4 px-6 rounded-lg transition duration-300">
               <img src={'/logo.png'} alt="Dattero logo" className='h-12' />
