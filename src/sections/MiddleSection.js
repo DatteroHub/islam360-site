4d500a7813b759752d3c01e508c84640bdf58a17
DatteroMaster <73127774+abouzahra@users.noreply.github.com>
2023-05-07 20:28:06 +0200

    small fix

--- src/sections/MiddleSection.js
+++ src/sections/MiddleSection.js
@@ -14,7 +14,7 @@ export default function MiddleSection() {
                     <Card src="/follow mentoring.jpg" title="formazione e mentoring" />
                 </div>
 
-                <div className="mt-24 mb-12 text-center font-medium text-3xl text-sky-500" id="contacts">Come contattarci</div>
+                <div className="pt-24 mb-12 text-center font-medium text-3xl text-sky-500" id="contacts">Come contattarci</div>
                 <div className='flex flex-col md:flex-row gap-24 justify-center items-center'>
                     <div className='flex justify-center items-center text-md h-40 w-56 bg-sky-100'>whatsapp</div>
                     <div className='flex justify-center items-center text-md h-40 w-56 bg-sky-100'>telegram</div>
