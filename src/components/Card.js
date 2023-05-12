8a277c0eee3c2f73462ef3a07851c7ea6e8d549d
DatteroMaster <73127774+abouzahra@users.noreply.github.com>
2023-05-13 00:22:36 +0200

    removed bg fixed and some graphic enhancements

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Card({ className , src, title }) {
   return (
     <div style={{ backgroundImage: `url("${src}")` }} className={`h-96 xl:h-128 w-80 xl:w-96 bg-cover 
-            rounded-xl outline-dashed outline-2 outline-sky-500
-            outline-offset-4 ${className}`}>
+            rounded-xl ${className}`}>
         <div className="font-normal text-xl xl:text-2xl 
             text-sky-50 uppercase bg-sky-500 py-2 px-4 rounded-t-xl">
             <div className='text-center'>{ title }</div>
--- src/pages/404.js
+++ src/pages/404.js
@@ -10,8 +10,8 @@ export default function NotFound() {
       <BgDiv className="py-24 mt-12">
         <ContentDiv className="flex justify-center items-center gap-40">
           <div>
-            <h1 className="font-semibold text-8xl md:text-9xl text-gray-700">404</h1>
-            <div className="font-medium text-2xl text-gray-500 mt-6">Pagina non trovata</div>
+            <h1 className="font-semibold text-center text-8xl md:text-9xl text-gray-700">404</h1>
+            <div className="font-medium text-center text-2xl text-gray-500 mt-6">Pagina non trovata</div>
           </div>
           <img src={'/404.svg'} alt="Hero" className='h-56 xl:h-64 hidden lg:flex' />
         </ContentDiv>
@@ -19,7 +19,6 @@ export default function NotFound() {
       <BgDiv className="pt-8 pb-24">
         <ContentDiv className="flex justify-center">
           <Link to="/" className="text-medium text-gray-500 flex items-center gap-4 bg-neutral-200/50 hover:bg-neutral-200 py-4 px-6 rounded-lg transition duration-300">
-              <img src={'/logo.png'} alt="Dattero logo" className='h-12' />
               <div>Torna alla Home</div>
           </Link>
         </ContentDiv>
